3b497b9899c44928c6f54383df7604a26323038a
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-10-05 11:42:50 +0200

    Dodaj pliki projektów.

---
+++ ControlsDemo/InputControlsDemo.xaml.cs
@@ -0,0 +1,47 @@
+namespace ControlsDemo;
+
+public partial class InputControlsDemo : ContentPage
+{
+	public InputControlsDemo()
+	{
+		InitializeComponent();
+	}
+
+	private	void OnCheckBoxCheckedChanged(object sender, EventArgs e)
+	{
+	
+
+		if ( check1.IsChecked )
+		{
+
+			etykieta.Text = "Zaznaczony";
+		}
+		else
+		{
+            etykieta.Text = "Brak";
+        }
+	}
+
+     private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+     {
+		if (slider.Value > 50)
+		{
+			
+				lblSlider.Text = "pozycja na ekranie wiêksza od 50: " + slider.Value.ToString();
+		}
+		else
+		{
+            lblSlider.Text = "pozycja na ekranie mniejsza od 50: " + slider.Value.ToString();
+
+        }
+
+    }
+	
+     private void stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+     {
+		  if (stepper != null)
+		  {
+			   lblSlider.Text = stepper.Value.ToString();
+		  }
+	 }
+}
